4e0fd5996bd6f704ddf3d31253ca2c14cb297a34
Hunter Jefferson <hj@artkade.com>
2021-04-03 18:13:49 -0500

    Removes routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Route, HashRouter } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+// import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 // components
 import { View1, View2, View3 } from './components'
@@ -8,16 +9,28 @@ import { View1, View2, View3 } from './components'
 import './App.sass';
 
 export const App: React.FC = () => {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (page) return
+  }, [page]);
+
   return (
-    <HashRouter basename="/">
-      {/* <div className="main">
-        <View1 />
+      <div className="main">
+        <div>
+          <div onClick={() => setPage(1)}>1</div>
+          <div onClick={() => setPage(2)}>2</div>
+          <div onClick={() => setPage(3)}>3</div>
+        </div>
+        {page == 1 ? <View1/> : null}
+        {page == 2 ? <View2/> : null}
+        {page == 3 ? <View3/> : null}
+        {/* <View1 />
         <View2 />
-        <View3 />
-      </div> */}
-      <Route path="/" exact component={View1} />
+        <View3 /> */}
+        {/* <Route path="/" exact component={View1} />
       <Route path="/2" component={View2} />
-      <Route path="/3" component={View3} />
-    </HashRouter>
+      <Route path="/3" component={View3} />  */}
+      </div> 
   );
 }
