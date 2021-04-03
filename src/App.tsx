78ac5699cab16b6462405f74a54c8a6b96fdfb19
Hunter Jefferson <hj@artkade.com>
2021-04-03 17:56:46 -0500

    Adds HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 // components
 import { View1, View2, View3 } from './components'
@@ -9,15 +9,15 @@ import './App.sass';
 
 export const App: React.FC = () => {
   return (
-    <Router>
+    <HashRouter basename="/">
       {/* <div className="main">
         <View1 />
         <View2 />
         <View3 />
       </div> */}
-      <Route path="/sunroom-test1" exact component={View1} />
-      <Route path="/sunroom-test1/2" component={View2} />
-      <Route path="/sunroom-test1/3" component={View3} />
-    </Router>
+      <Route path="/" exact component={View1} />
+      <Route path="/2" component={View2} />
+      <Route path="/3" component={View3} />
+    </HashRouter>
   );
 }
