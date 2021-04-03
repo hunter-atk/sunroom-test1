d9cd535b005535bc69c27fde16c5122cbe4b222a
Hunter Jefferson <hj@artkade.com>
2021-04-03 17:45:46 -0500

    Fixes React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 // components
 import { View1, View2, View3 } from './components'
@@ -8,10 +9,15 @@ import './App.sass';
 
 export const App: React.FC = () => {
   return (
-    <div className="main">
-      <View1 />
-      <View2 />
-      <View3 />
-    </div>
+    <Router>
+      {/* <div className="main">
+        <View1 />
+        <View2 />
+        <View3 />
+      </div> */}
+      <Route path="/" exact component={View1} />
+      <Route path="/2" component={View2} />
+      <Route path="/3" component={View3} />
+    </Router>
   );
 }
